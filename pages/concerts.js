17972741c07c1879eb649035e5155ad78bbb6bb8
Woo <wychoi356@gmail.com>
2020-05-14 21:53:43 -0400

    adding pages placeholder for /concerts and /gallery

---
+++ pages/concerts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Layout from "../src/components/Layout";
+
+export default function concerts() {
+  return (
+    <Layout>
+      <h1>CONCERTS PAGE</h1>
+    </Layout>
+  );
+}
