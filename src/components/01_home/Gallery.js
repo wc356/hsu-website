b7e7910db6a03c6450c194c4c9d2c0d882ac4ae6
Woo <wychoi356@gmail.com>
2020-08-29 18:45:45 -0400

    fix homepage–gallery mobile display & mobile

--- src/components/01_home/Gallery.js
+++ src/components/01_home/Gallery.js
@@ -12,6 +12,52 @@ const Gallery = () => {
         {pics.map(({ id, picName }) => (
           <a key={id} href={`${GAL_ROOT}/${picName}`} className={`img-${id}`} />
         ))}
+        <style jsx>
+          {`
+            .img-1 {
+              grid-area: img-1;
+              min-height: 51.5rem;
+              background-image: url("${GAL_ROOT}/IMG_0001.jpg");
+            }
+            .img-2 {
+              grid-area: img-2;
+              background-image: url("${GAL_ROOT}/IMG_0002.jpg");
+            }
+            .img-3 {
+              grid-area: img-3;
+              background-image: url("${GAL_ROOT}/IMG_0003.jpg");
+            }
+            .img-4 {
+              grid-area: img-4;
+              background-image: url("${GAL_ROOT}/IMG_0004.jpg");
+            }
+            .img-5 {
+              grid-area: img-5;
+              min-height: 51.5rem;
+              background-image: url("${GAL_ROOT}/IMG_0005.jpg");
+            }
+            .img-6 {
+              grid-area: img-6;
+              background-image: url("${GAL_ROOT}/IMG_0006.jpg");
+            }
+            .img-7 {
+              grid-area: img-7;
+              background-image: url("${GAL_ROOT}/IMG_0007.jpg");
+            }
+            .img-8 {
+              grid-area: img-8;
+              background-image: url("${GAL_ROOT}/IMG_0008.jpg");
+            }
+            .img-9 {
+              grid-area: img-9;
+              background-image: url("${GAL_ROOT}/IMG_0009.jpg");
+            }
+            .img-10 {
+              grid-area: img-10;
+              background-image: url("${GAL_ROOT}/IMG_0010.jpg");
+            }
+          `}
+        </style>
       </div>
     );
   }
